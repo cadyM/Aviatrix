837230dc03ac08d0e1d9c5107652d8f68d7c9092
Apple <apple@Apples-MacBook-Pro-46.local>
2019-07-10 13:57:03 -0500

    Complete Iteartion0

--- AviatrixApp/Aviatrix.swift
+++ AviatrixApp/Aviatrix.swift
@@ -8,10 +8,22 @@
 
 import Foundation
 
+
 class Aviatrix {
+    //Saying who created the plane and airline    var author = " "
+    var author = " "
     
-    func start() -> Bool {
-        return true
+    init(myAuthor:String){
+        author = myAuthor
+    }
+   
+    //This Function should tell us if the airplane is running or not
+    var running = false
+    
+    //Starts the plane so it is running
+    func start() -> Bool { //Book stand for boolean which means true or false
+        running = true
+        return running
     }
     
     func refuel() {
